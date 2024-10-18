adc460b4b9527f74b9eaf27d79594609be57f4aa
Bre Humphres <breannahumphres@gmail.com>
2024-10-17 17:29:27 -0700

    all models and relationships created

---
+++ server/migrations/versions/6f4544530237_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 6f4544530237
+Revises: 
+Create Date: 2024-10-17 16:25:12.555591
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6f4544530237'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
